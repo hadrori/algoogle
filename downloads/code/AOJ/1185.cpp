c42d6eacc76363db99d095945aedd100336a35ac
hadrori <hadrori.hs@gmail.com>
2014-05-05 00:39:43 +0900

    Site updated at 2014-05-04 15:39:43 UTC

--- downloads/code/AOJ/1185.cpp
+++ downloads/code/AOJ/1185.cpp
@@ -1,38 +1,13 @@
-#include <cstring>
 #include <algorithm>
-#include <cstdio>
 #include <iostream>
 #include <vector>
-#include <map>
-#include <set>
-#include <complex>
-#include <queue>
-#include <stack>
-#include <string>
-#include <cmath>
-#include <bitset>
-
-#define rep(i,a) for(int i = 0;i < (a); i++)
-#define repi(i,a,b) for(int i = (a); i < (b); i++)
-#define repd(i,a,b) for(int i = (a); i >= (b); i--)
-#define repit(i,a) for(__typeof((a).begin()) i = (a).begin(); i != (a).end(); i++)
-#define all(u) (u).begin(),(u).end()
-#define rall(u) (u).rbegin(),(u).rend()
-#define UNIQUE(u) (u).erase(unique(all(u)),(u).end())
-#define pb push_back
-#define mp make_pair
-#define lb lower_bound
-#define ub upper_bound
-#define INF 1e9
-#define EPS 1e-10
-#define PI acos(-1.0)
 
 using namespace std;
+#define rep(i,a) for(int i = 0;i < (a); i++)
+#define repi(i,a,b) for(int i = (a); i < (b); i++)
+#define pb push_back
 
-typedef long long ll;
 typedef vector<int> vi;
-typedef pair<int,int> pii;
-typedef complex<double> P;
 
 struct Line{
     bool tate;
@@ -155,17 +130,6 @@ int main(){
 	    if(crossline(lines[i],lines[j])) add_edge(i,j);
 	}
 	V = lines.size();
-	/*
-	cout << "V size = " << V << endl;
-	rep(i,V){
-	    cout << "v = " << i << " : ";
-	    cout << "(" << lines[i].sx << "," << lines[i].sy << ") to ";
-	    cout << "(" << lines[i].tx << "," << lines[i].ty << ")" << endl;;
-	    rep(j,G[i].size()) cout << G[i][j] << ' ';
-	    cout << endl;
-	}
-	cout << "ans = ";
-	*/
 	cout << corner() - lines.size() + bipartie_matching() + 1 << endl;
     }
     return 0;
