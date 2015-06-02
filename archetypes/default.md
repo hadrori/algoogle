69f9d831b83f2897cd64d1f2df256da7b11f4e4e
hadrori <hadrori.hs@gmail.com>
2015-06-03 00:02:17 +0900

    Remoce hugo parts

--- archetypes/default.md
+++
@@ -1,5 +0,0 @@
-+++
-Description = ""
-tags = []
-category = []
-+++
--- static/code/PE/77.hs
+++
@@ -1,8 +0,0 @@
-import Math.NumberTheory.Primes
-n = 5000
-solver h res
-  | res < 0 = 0
-  | res == 0 = 1
-  | otherwise = sum [solver x (res-x) | x <- takeWhile (<= h) primes]
-main = print (head [x| x <- [0..], solver x x > n])
-
