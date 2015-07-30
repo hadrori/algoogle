43cda44c0831e3b4ba941029a1e0bee3347585b7
hadrori <hadrori.hs@gmail.com>
2015-07-30 15:13:59 +0900

    Fix uel

--- plugins/post.rb
+++ plugins/post.rb
@@ -1,5 +1,10 @@
 module Jekyll
   class Post
+    alias_method :old_url, :url
+    def url
+      old_url.downcase
+    end
+
     def populate_categories
       categories_from_data = Utils.pluralized_array_from_hash(data, 'category', 'categories')
       self.categories = (
