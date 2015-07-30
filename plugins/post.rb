4599e2d1dc51e31885430e53d7815ad1f465058e
hadrori <hadrori.hs@gmail.com>
2015-07-30 14:39:01 +0900

    update category name

---
+++ plugins/post.rb
@@ -0,0 +1,10 @@
+module Jekyll
+  class Post
+    def populate_categories
+      categories_from_data = Utils.pluralized_array_from_hash(data, 'category', 'categories')
+      self.categories = (
+        Array(categories) + categories_from_data
+      ).flatten.uniq
+    end
+  end
+end
